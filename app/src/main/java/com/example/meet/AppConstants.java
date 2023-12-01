2ae7033eab0c2ca0aa13a118f69ef62c1a6baf7d
Crossquin12 <suci.22068@mhs.unesa.ac.id>
2023-12-01 07:54:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/meet/AppConstants.java
@@ -0,0 +1,6 @@
+package com.example.meet;
+
+public class AppConstants {
+    public static long appId = 1076897261;
+    public static String appSign = "159a6a1d362df977224e5e5b614c9a49cc1e7c31b58b22302c25cbcfb77b22ed";
+}
